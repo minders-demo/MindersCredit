59f0427f03cca5a18d294bc6372b678296490d52
minders-demo <sergio.gomez@minders.io>
2026-07-14 12:25:55 -0500

    Update patch_demolab.cjs

--- patch_demolab.cjs
+++ patch_demolab.cjs
@@ -3,7 +3,7 @@ let code = fs.readFileSync('src/views/DemoLab.tsx', 'utf-8');
 
 code = code.replace(
   'const [apiKey, setApiKey] = useState((import.meta as any).env.VITE_AMPLITUDE_API_KEY || "");',
-  'const [apiKey, setApiKey] = useState((import.meta as any).env.VITE_AMPLITUDE_API_KEY || "cce51808526c9ce95082ce3d4ac97106");'
+  'const [apiKey, setApiKey] = useState((import.meta as any).env.VITE_AMPLITUDE_API_KEY || "714214e5842a299f3fa47e23db5fe728");'
 );
 
 fs.writeFileSync('src/views/DemoLab.tsx', code);
